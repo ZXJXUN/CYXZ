17feae89e2eae1a334649f0294d72f58a7b3365a
liuxiongzhuo <3231729413@qq.com>
2024-11-30 18:23:34 +0800

    刘雄卓接了后端接口，重写了前端，无ui库
    刘雄卓接了后端接口，重写了前端，无ui库

--- pages/newQuestion/newQuestion.js
+++ pages/newQuestion/newQuestion.js
@@ -1,5 +1,25 @@
-import Notify from '@vant/weapp/notify/notify'
 Page({
+  previewPicture: function (event) {
+    wx.previewImage({
+      urls: this.data.tempPictures,
+      current: event.currentTarget.dataset.picture
+    })
+  },
+  chooseSubject: function (event) {
+    this.setData({
+      choosedSubject: event.currentTarget.dataset.subjectIndex
+    })
+  },
+  titleInput: function (event) {
+    this.setData({
+      title: event.detail.value
+    })
+  },
+  contentInput: function (event) {
+    this.setData({
+      content: event.detail.value
+    })
+  },
   submitToDraft: async function () {
     let promises = [];
     const pictures = this.data.tempPictures
@@ -7,7 +27,6 @@ Page({
     for (let i = 0; i < pictures.length; i++) {
       let element = pictures[i];
       if (element.split(':')[0] == 'https') {
-        // console.log('element: '+element);
         submitPictures.push(element)
       } else {
         promises.push(new Promise((resolve, reject) => {
@@ -16,13 +35,11 @@ Page({
             name: 'file',
             url: "http://47.120.26.83:8000/oss/upload",
             header: {
-              'username': 'ab',
-              'token': '6927fe4d-a141-47e6-9e11-faf68d2ab601'
+              'username': wx.getStorageSync('name'),
+              'token': wx.getStorageSync('token'),
             },
             success: (res) => {
               submitPictures.push('https://oss-symbol.oss-cn-beijing.aliyuncs.com/' + JSON.parse(res.data).data)
-              // console.log('submitPictures: '+submitPictures.length)
-              // console.log('res.data: '+res.data)
               resolve(res)
             },
             fail: (err) => {
@@ -101,7 +118,6 @@ Page({
     })
   },
   deletePicture: function (event) {
-    // console.log(this.data.tempPictures);
     const {
       tempPictures
     } = this.data
@@ -129,41 +145,57 @@ Page({
       tempPictures
     })
   },
-  showSubjectPicker: function () {
-    this.setData({
-      isShowSubjectPicker: !this.data.isShowSubjectPicker
-    })
-  },
-  subjectChoose: function (event) {
-    this.showSubjectPicker()
-    this.setData({
-      choosedSubject: event.detail.index
-    })
-    // console.log(this.data.choosedSubject);
-  },
+  // validate: function (title, message) {
+  //   await new Promise((resolve,reject)=>{
+  //     if (!this.data[title]) {
+  //       wx.showToast({
+  //         title: message,
+  //       })
+  //     }
+  //   })
+  //   return Promise.resolve()
+  // },
   submit: async function () {
+    // await this.validate('title','标题为空')
+    // await this.validate('content','内容为空')
+    if (!this.data.title) {
+      console.log(this.data.title);
+      wx.showToast({
+        title: '题目为空',
+        icon:'error'
+      })
+      return
+    }
+    if (!this.data.content) {
+      wx.showToast({
+        title: '内容为空',
+        icon:'error'
+      })
+      return
+    }
     let promises = [];
     const pictures = this.data.tempPictures
     let submitPictures = this.data.submitPictures
     for (let i = 0; i < pictures.length; i++) {
       let element = pictures[i];
       if (element.split(':')[0] == 'https') {
-        // console.log('element: '+element);
         submitPictures.push(element)
       } else {
         promises.push(new Promise((resolve, reject) => {
           wx.uploadFile({
             filePath: element,
             name: 'file',
-            url: "http://47.120.26.83:8000/oss/upload",
+            url: "https://nurl.top:8000/oss/upload",
             header: {
-              'username': 'ab',
-              'token': '6927fe4d-a141-47e6-9e11-faf68d2ab601'
+              'username': wx.getStorageSync('name'),
+              'token': wx.getStorageSync('token')
             },
             success: (res) => {
-              submitPictures.push('https://oss-symbol.oss-cn-beijing.aliyuncs.com/' + JSON.parse(res.data).data)
-              // console.log('submitPictures: '+submitPictures.length)
-              // console.log('res.data: '+res.data)
+              // submitPictures.push('https://oss-symbol.oss-cn-beijing.aliyuncs.com/' + (JSON.parse(res.data)).data)
+              submitPictures.push((JSON.parse(res.data)).data)
+              this.setData({
+                submitPictures: submitPictures
+              })
               resolve(res)
             },
             fail: (err) => {
@@ -176,72 +208,87 @@ Page({
     }
     await Promise.all(promises).then(() => {
       wx.request({
-        url: 'http://47.120.26.83:8000/api/answerly/v1/question',
-        // url: 'http://127.0.0.1:3000',
+        // url: 'http://localhost:4000',
+        url: 'https://nurl.top:8000/api/answerly/v1/question',
         header: {
-          'username': 'ab',
-          'token': '6927fe4d-a141-47e6-9e11-faf68d2ab601',
-          'Content-Type': 'application/json'
+          'token': wx.getStorageSync('token'),
+          'username': wx.getStorageSync('name')
         },
         method: 'POST',
         data: {
           "content": this.data.content,
           "title": this.data.title,
-          "user_id": 0,
-          "category": this.data.choosedSubject,
-          "pictures": this.data.submitPictures
+          // "categoryId": this.data.subjectList[this.data.choosedSubject],
+          "categoryId": '1862378922353868801',
+          "images": this.data.submitPictures.join(',')
         },
         success: (res) => {
-          // console.log(res.data)
-          Notify({
-            type: 'success',
-            message: '提交成功',
-            selector: '#van-notify',
-            context: this
-          });
-          this.setData({
-            isShowSubjectPicker: false,
-            tempPictures: [],
-            choosedSubject: 0,
-            content: '',
-            title: '',
-            submitPictures: []
-          })
-          Notify({
-            type: 'success',
-            message: '提交成功',
-            selector: '#van-notify',
-            context: this
-          });
+          console.log(res.data)
+          if (res.data.code!=="0") {
+            wx.showToast({
+              title: '发布失败',
+              icon: 'error'
+            })
+            
+          } else {
+            
+            wx.showToast({
+              title: '发布成功',
+            })
+            this.setData({
+              isShowSubjectPicker: false,
+              tempPictures: [],
+              choosedSubject: 0,
+              content: '',
+              title: '',
+              submitPictures: []
+            })
+          }
         },
         fail: (err) => {
+          if (res.data.code) {
+            wx.showToast({
+              title: '发布失败',
+              icon: 'error'
+            })
+          }
           console.log(err);
-          Notify({
-            type: 'warning',
-            message: '提交失败',
-            selector: '#van-notify',
-            context: this
-          });
         }
       })
     })
   },
   data: {
-    isShowSubjectPicker: false,
     tempPictures: [],
-    subjectList: ['高等代数', '基础物理', '离散数学', '数据结构', '程序设计'],
+    subjectList: [],
     choosedSubject: 0,
     content: '',
     title: '',
     submitPictures: []
   },
   onLoad: function (options) {
-    let draft = JSON.parse(options.draft)
-    this.setData({
-      choosedSubject: draft.draft_category,
-      title: draft.draft_title,
-      content: draft.draft_content,
-      tempPictures: draft.draft_pictures
+    if (Object.keys(options).length!==0) {
+      let draft = JSON.parse(options.draft)
+      this.setData({
+        choosedSubject: draft.draft_category,
+        title: draft.draft_title,
+        content: draft.draft_content,
+        tempPictures: draft.draft_pictures
+      })
+    }
+    wx.request({
+      url: 'https://nurl.top:8000/api/answerly/v1/category',
+      header: {
+        'token': wx.getStorageSync('token'),
+        'username': wx.getStorageSync('name')
+      },
+      success: (res) => {
+        this.setData({
+          subjectList: res.data.data
+        })
+      },
+      fail: (err) => {
+        console.log('err: '+err);
+      }
     })
   }
 });
