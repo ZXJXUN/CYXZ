2637aaf71ca8a2a3fdef9a01d8061ac759113958
liuxiongzhuo <3231729413@qq.com>
2024-12-05 09:31:10 +0800

    刘雄卓改了改index，加了图片
    刘雄卓改了改index，加了图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,32 @@
 Page({
+  data:{
+    subjects:[]
+  },
+  onReady:function () {
+    wx.request({
+      url: 'https://nurl.top:8000/api/answerly/v1/category',
+      header: {
+        'token': wx.getStorageSync('token'),
+        'username': wx.getStorageSync('name')
+      },
+      success:(res)=>{
+        this.setData({subjects:res.data.data})
+      }
+    })
+  },
   // 处理点击科目跳转
   onNavigate: function (event) {
+    console.log(123);
     const selectedSubject = event.currentTarget.dataset.subject; // 获取点击的科目名称
-
-    // 根据选中的科目跳转
-    if (selectedSubject === '数学分析' || selectedSubject === '高等代数' || selectedSubject === '程序设计' || selectedSubject === '基础物理' || selectedSubject === '数据结构' || selectedSubject === '离散数学') {
       wx.navigateTo({
         url: '/pages/list/list', // 这里是跳转的目标页面
       });
-    }
+
+    // 根据选中的科目跳转
+    // if (selectedSubject === '数学分析' || selectedSubject === '高等代数' || selectedSubject === '程序设计' || selectedSubject === '基础物理' || selectedSubject === '数据结构' || selectedSubject === '离散数学') {
+    //   wx.navigateTo({
+    //     url: '/pages/list/list', // 这里是跳转的目标页面
+    //   });
+    // }
   }
 });
